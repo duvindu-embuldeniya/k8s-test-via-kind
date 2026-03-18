8db9e934ba728a29fe589abf6f9518b6add5f294
duvindu-embuldeniya <duvindu.personal@gmail.com>
2026-03-18 23:31:16 +0530

    k8s day1

--- simply/simply/settings.py
+++ simply/simply/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--@u)%e+8o@tfhle%^np*ydyh&-1ybd3gpsl=ko)nu07w=v6n*&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
